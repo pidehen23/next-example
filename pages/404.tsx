90fe562c896a54bceb052cbcbbf2d4bb94b8203d
chen jiajing <16602132375@163.com>
2021-09-23 23:58:20 +0800

    feat: custom 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
-// pages/404.js
+import router from 'next/router';
+
 export default function Custom404() {
   return  (
   <div className="w-screen h-screen flex justify-center items-center">
-    <h1>404 - Page Not Found，bay bay!</h1>
+    <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+    <button className="h-8 bg-blue-400 rounded-full px-4 ml-2 text-white" onClick={()=>router.push('/')}>page reload</button>
   </div>)
 }
